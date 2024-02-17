dd9e7f2c77db5ed137515fdfaf78df55be66e81a
leanhduy <laduy.hcmus@gmail.com>
2024-02-17 17:44:25 +0700

    add Category model

---
+++ BulkyWeb/Models/Category.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BulkyWeb.Models;
+
+public class Category
+{
+	[Key]
+	public int ID { get; set; }
+	[Required]
+	public string Name {  get; set; }	
+	public int DisplayedOrder { get; set; }
+}
