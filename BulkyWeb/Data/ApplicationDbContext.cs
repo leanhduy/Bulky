b4fc542e2fb3eca0018ea203357d54d36f97b0bc
leanhduy <laduy.hcmus@gmail.com>
2024-02-18 01:55:32 +0700

    Seed data for Categories table

--- BulkyWeb/Data/ApplicationDbContext.cs
+++ BulkyWeb/Data/ApplicationDbContext.cs
@@ -9,5 +9,14 @@ namespace BulkyWeb.Data
 
 		// Create DbSet (equivalent to tables in database)
 		public DbSet<Category> Categories { get; set; }
+
+		protected override void OnModelCreating(ModelBuilder modelBuilder)
+		{
+			modelBuilder.Entity<Category>().HasData(
+				new Category { ID = 1, Name = "Action", DisplayedOrder = 1 },
+				new Category { ID = 2, Name = "SciFi", DisplayedOrder = 2 },
+				new Category { ID = 3, Name = "History", DisplayedOrder = 3 }
+			);
+		}
 	}
 }
