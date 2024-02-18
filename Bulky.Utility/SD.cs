18258f8116195e3820f3af237080e1b3af2fb168
leanhduy <laduy.hcmus@gmail.com>
2024-02-19 00:04:10 +0700

    Redesign web app into n-tiers application
    create 3 library classes for Models, Utility & DataAccess
    
    Bulky.Models: storing models
    
    Bulky.DataAccess: storing DbContext subclass and Migration files
    
    Utility: add new class SD.cs for Static Details
    
    install necessary packages for each project
    
    update the namespace and add dependencies between projects

---
+++ Bulky.Utility/SD.cs
@@ -0,0 +1,10 @@
+﻿namespace Bulky.Utility
+{
+	/// <summary>
+	/// SD = Static Details
+	/// Handles all the static files / content of the web app
+	/// </summary>
+	public static class SD
+	{
+	}
+}
