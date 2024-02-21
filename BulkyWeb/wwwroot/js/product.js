994cf132e476ae17f0e3c46d3f914fb224ddf758
leanhduy <laduy.hcmus@gmail.com>
2024-02-22 00:01:49 +0700

    Product: Adding the Edit & Delete button via js

--- BulkyWeb/wwwroot/js/product.js
+++ BulkyWeb/wwwroot/js/product.js
@@ -6,12 +6,27 @@ function loadTable() {
     dataTable = $('#productTable').DataTable({
         "ajax": { url: '/admin/product/getall' },
         "columns": [
-            { "data": "isbn" },
-            { "data": "title" },
-            { "data": "author" },
-            { "data": "listPrice" },
-            { "data": "description" },
-            { "data": "category.name" }
+            { "data": 'isbn', "width": "5%" },
+            { "data": 'title', "width": "20%" },
+            { "data": 'author', "width": "10%" },
+            { "data": 'listPrice', "width": "5%" },
+            { "data": 'description', "width": "25%" },
+            { "data": 'category.name', "width": "5%" },
+            // Edit button
+            {
+                "data": 'id',
+                "render": function (data) {
+                    return `
+                    <div class=w-75 btn-group" role="group">
+                    <a href="/admin/product/upsert?id=${data}" class="btn btn-primary mx-2">
+                    <i class="bi bi-pencil-square"></i>Edit</a>
+                    <a href="/admin/product/delete?id=${data}" asp-controller="Product" asp-action="Delete" class="btn btn-danger mx-2">
+                    <i class="bi bi-trash"></i>Delete</a>
+                    </div>
+                    `;
+                },
+                "width": "30%"
+            }
         ]
     });
 }
